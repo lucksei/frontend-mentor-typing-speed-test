5b64c53395511d6c3869869daa00be3ce6fbdaa5
lucksei <luca6mandi@gmail.com>
2026-02-12 18:53:15 +0000

    working on game logic for typingTest.ts

--- src/components/AppTypingTest.vue
+++ src/components/AppTypingTest.vue
@@ -1,22 +1,81 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const text = ref('')
-text.value =
+import { onMounted, shallowRef, useTemplateRef } from 'vue'
+import TypingTest from '@/utils/typingTest'
+
+const textRef = useTemplateRef('text-ref')
+
+const testText =
   'The archaeological expedition unearthed artifacts that complicated prevailing theories about Bronze Age trade networks. Obsidian from Anatolia, lapis lazuli from Afghanistan, and amber from the Baltic—all discovered in a single Mycenaean tomb—suggested commercial connections far more extensive than previously hypothesized. \"We\'ve underestimated ancient peoples\' navigational capabilities and their appetite for luxury goods,\" the lead researcher observed. \"Globalization isn\'t as modern as we assume.\"'
+
+const typingTest = new TypingTest(testText)
+const textArray = shallowRef(typingTest.getText())
+
+const handleKeyPress = (event: KeyboardEvent) => {
+  event.preventDefault()
+  if (['Control', 'Shift', 'Meta', 'Escape', 'Tab'].includes(event.key)) return
+  if (event.key === 'Backspace') {
+    typingTest.sendBackspace()
+    textArray.value = [...typingTest.getText()]
+    return
+  }
+  if (event.key === ' ') {
+    typingTest.sendSpace()
+    textArray.value = [...typingTest.getText()]
+    return
+  }
+  typingTest.sendKeyStroke(event.key)
+  textArray.value = [...typingTest.getText()]
+}
+onMounted(() => {
+  if (textRef.value) {
+    textRef.value.addEventListener('keydown', handleKeyPress)
+  }
+})
 </script>
 
 <template>
   <div class="typing-test-container">
-    <div class="typing-test">{{ text }}</div>
+    <p class="text" tabindex="-1" ref="text-ref">
+      <span class="word" :class="word.status" v-for="word in textArray" :key="word.id">
+        <span class="char" :class="char.status" v-for="char in word.word" :key="char.id">
+          {{ char.char }}
+        </span>
+      </span>
+    </p>
   </div>
 </template>
 
 <style scoped>
 .typing-test-container {
+  display: flex;
+  flex-direction: column;
   padding: 1rem 0 1rem 0;
 }
-.typing-test {
+.text {
   font-size: var(--font-size-large);
+  /* color: var(--colors-neutral-500); */
+}
+.word::after {
+  content: ' ';
+}
+
+.char.empty {
+  color: var(--colors-neutral-500);
+}
+
+.char.correct {
+  color: var(--colors-green-500);
+}
+
+.char.incorrect {
+  color: var(--colors-red-500);
+  text-decoration: underline;
+  text-underline-offset: 0.4rem;
+  text-decoration-thickness: 0.2rem;
+}
+
+.char.selected {
+  background-color: var(--colors-neutral-800);
   color: var(--colors-neutral-500);
 }
 </style>
--- src/utils/typingTest.ts
+++ src/utils/typingTest.ts
@@ -1,10 +1,130 @@
+type Character = {
+  id: number
+  char: string
+  status: 'empty' | 'correct' | 'incorrect' | 'selected'
+}
+
+type Word = {
+  id: number
+  word: Character[]
+  status: 'empty' | 'correct' | 'incorrect'
+}
+
+type Cursor = {
+  word_idx: number
+  char_idx: number | 'end'
+}
+
 class TypingTest {
-  words: string[]
+  text: Word[]
+  cursor: Cursor
   constructor(text: string) {
-    this.words = this._textToWordlist(text)
+    this.text = this._textToWordlist(text)
+    this.cursor = { word_idx: 0, char_idx: 0 }
+    this._cursorReset()
   }
 
-  _textToWordlist(text: string): string[] {
-    return text.split(' ')
+  _wordToCharacterList(word: string): Character[] {
+    return word.split('').map((char, index) => ({
+      id: index,
+      char,
+      status: 'empty',
+    }))
+  }
+
+  _textToWordlist(text: string): Word[] {
+    const words = text.split(' ')
+    return words.map((word, index) => ({
+      id: index,
+      word: this._wordToCharacterList(word),
+      status: 'empty',
+    }))
+  }
+
+  _cursorReset() {
+    this.cursor = { word_idx: 0, char_idx: 0 }
+
+    if (!this.text[0]) throw new Error('Text is empty')
+    if (!this.text[0].word[0]) throw new Error('Text is empty')
+    this.text[0].word[0].status = 'selected'
+  }
+
+  _cursorAdvance(status: 'correct' | 'incorrect'): number {
+    const curr_word_length = this.text[this.cursor.word_idx]!.word.length
+
+    if (typeof this.cursor.char_idx === 'number') {
+      this.text[this.cursor.word_idx]!.word[this.cursor.char_idx]!.status = status
+      if (this.cursor.char_idx === curr_word_length - 1) {
+        this.cursor.char_idx = 'end'
+        return 1
+      }
+      this.cursor.char_idx += 1
+      this.text[this.cursor.word_idx]!.word[this.cursor.char_idx]!.status = 'selected'
+    }
+    if (this.cursor.char_idx === 'end') return 1
+
+    return 0
+  }
+
+  _cursorAdvanceWord(): number {
+    const curr_text_length = this.text.length
+
+    if (typeof this.cursor.char_idx === 'number') {
+      this.text[this.cursor.word_idx]!.word[this.cursor.char_idx]!.status = 'empty'
+    }
+
+    // If the current word is the last word
+    if (this.cursor.word_idx === curr_text_length - 1) return 1
+
+    this.cursor.word_idx += 1
+    this.cursor.char_idx = 0
+    this.text[this.cursor.word_idx]!.word[this.cursor.char_idx]!.status = 'selected'
+    return 0
+  }
+
+  _cursorBacktrack(): number {
+    if (typeof this.cursor.char_idx === 'number') {
+      this.text[this.cursor.word_idx]!.word[this.cursor.char_idx]!.status = 'empty'
+      if (this.cursor.char_idx === 0) {
+        this.cursor.word_idx -= 1
+        this.cursor.char_idx = 'end'
+        return 0
+      }
+      this.cursor.char_idx -= 1
+      this.text[this.cursor.word_idx]!.word[this.cursor.char_idx]!.status = 'selected'
+      return 0
+    }
+    if (this.cursor.char_idx === 'end') {
+      this.cursor.char_idx = this.text[this.cursor.word_idx]!.word.length - 1
+      this.text[this.cursor.word_idx]!.word[this.cursor.char_idx]!.status = 'selected'
+      return 0
+    }
+    return 0
+  }
+
+  getText(): Word[] {
+    return this.text
+  }
+
+  sendKeyStroke(key: string) {
+    if (typeof this.cursor.char_idx !== 'number') return
+    const current_char = this.text[this.cursor.word_idx]?.word[this.cursor.char_idx]
+    if (!current_char) return
+
+    if (current_char.char === key) {
+      this._cursorAdvance('correct')
+    } else {
+      this._cursorAdvance('incorrect')
+    }
+  }
+
+  sendSpace() {
+    if (this.cursor.char_idx === 'end') this._cursorAdvanceWord()
+  }
+
+  sendBackspace() {
+    this._cursorBacktrack()
   }
 }
+
+export default TypingTest
