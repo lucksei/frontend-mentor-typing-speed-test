f1a05ef14b5564c174a294e6412f8748b076c53c
lucksei <luca6mandi@gmail.com>
2026-02-24 19:25:27 +0000

    fixing up test completed icon

--- src/components/TestCompletedModal.vue
+++ src/components/TestCompletedModal.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue'
 import IconRestart from './images/IconRestart.vue'
+import IconCompleted from './images/IconCompleted.vue'
 
 const props = defineProps<{
   isHidden?: boolean
@@ -29,21 +30,28 @@ watchEffect(() => {
 <template>
   <div class="overlay">
     <div class="test-completed-modal" :class="`${fadeState} ${isHiddenClass ? 'hidden' : ''}`">
+      <div class="icon">
+        <IconCompleted />
+      </div>
       <h2>Baseline Established!</h2>
       <p class="text-secondary">
-        You've set the bar. Now the real challenge begins-time to beat it.
+        You've set the bar. Now the real challenge begins—time to beat it.
       </p>
       <div class="stat-container">
-        <span>WPM:</span>
-        <span>85</span>
+        <span class="stat-name text-secondary">WPM:</span>
+        <span class="stat-value">85</span>
       </div>
       <div class="stat-container">
-        <span>Accuracy:</span>
-        <span>90%</span>
+        <span class="stat-name text-secondary">Accuracy:</span>
+        <span class="stat-value red">90%</span>
       </div>
       <div class="stat-container">
-        <span>Characters:</span>
-        <span>120/5</span>
+        <span class="stat-name text-secondary">Characters:</span>
+        <span class="stat-value">
+          <span class="green">120</span>
+          <span class="text-secondary">/</span>
+          <span class="red">5</span>
+        </span>
       </div>
       <button class="restart-button">Beat This Score<IconRestart /></button>
     </div>
@@ -62,6 +70,10 @@ watchEffect(() => {
   }
 }
 
+.fade-in {
+  animation: fade-in 1s ease-in-out;
+}
+
 @keyframes fade-out {
   0% {
     opacity: 1;
@@ -73,24 +85,83 @@ watchEffect(() => {
   }
 }
 
+.fade-out {
+  animation: fade-out 1s ease-in-out;
+}
+
 .overlay {
   position: relative;
 }
+
 .test-completed-modal {
   position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  gap: 1rem;
   background-color: var(--colors-neutral-900);
   z-index: 300;
   height: 100vh;
   left: -1rem;
   width: calc(100% + 2rem);
   padding: 1rem;
-}
 
-.fade-in {
-  animation: fade-in 1s ease-in-out;
-}
+  p,
+  h1 {
+    text-align: center;
+  }
 
-.fade-out {
-  animation: fade-out 1s ease-in-out;
+  .icon {
+    margin-bottom: 1rem;
+  }
+
+  .stat-container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: start;
+    border: 1px solid var(--colors-neutral-800);
+    border-radius: 0.5rem;
+    padding: 0.6rem 1rem 0.6rem 1rem;
+    gap: 0.5rem;
+
+    .stat-name {
+      font-size: var(--font-size-medium);
+    }
+
+    .stat-value {
+      font-size: var(--font-size-medium);
+      font-weight: var(--weight-bold);
+    }
+
+    .red {
+      color: var(--colors-red-500);
+    }
+
+    .green {
+      color: var(--colors-green-500);
+    }
+
+    .yellow {
+      color: var(--colors-yellow-400);
+    }
+  }
+
+  .restart-button {
+    padding: 0.8rem 1.1rem 0.8rem 1.1rem;
+    background-color: var(--colors-neutral-0);
+    font-weight: var(--weight-bold);
+    border: none;
+
+    img {
+      filter: invert();
+    }
+  }
+
+  .restart-button:hover {
+    box-shadow: inset 0 0 0 2px var(--colors-neutral-400);
+  }
 }
 </style>
--- src/components/images/IconCompleted.vue
+++ src/components/images/IconCompleted.vue
@@ -1,5 +1,34 @@
 <script setup lang="ts"></script>
 
 <template>
-  <img class="icon-image" src="/src/assets/images/icon-completed.svg" alt="icon-completed" />
+  <div class="shadow-1">
+    <div class="shadow-2">
+      <img src="/src/assets/images/icon-completed.svg" alt="icon-completed" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.shadow-1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3.1rem;
+  height: 3.1rem;
+  border-radius: 50%;
+  box-shadow: 0px 0px 0px 1rem hsla(140, 63%, 57%, 0.2);
+}
+.shadow-2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  box-shadow: 0px 0px 0px 0.6rem hsla(140, 63%, 57%, 0.2);
+}
+img {
+  width: 3.2rem;
+  height: 3.2rem;
+}
+</style>
